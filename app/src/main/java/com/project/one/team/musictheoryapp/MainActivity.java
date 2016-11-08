245ea6beb3adc8f81b43c2aa940756a9bcc0b591
Jacob <jacobcarey95@googlemail.com>
2016-11-08 13:53:10 +0000

    Second commit. JC.

--- app/src/main/java/com/project/one/team/musictheoryapp/MainActivity.java
+++ app/src/main/java/com/project/one/team/musictheoryapp/MainActivity.java
@@ -2,12 +2,31 @@ package com.project.one.team.musictheoryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button testButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find button by ID.
+        testButton = (Button) findViewById(R.id.testButtonId);
+        testButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                TextView txtView = (TextView) findViewById(R.id.testTextViewId);
+                txtView.setText("Would you look at that!");
+            }
+        });
+
     }
+
+
 }
