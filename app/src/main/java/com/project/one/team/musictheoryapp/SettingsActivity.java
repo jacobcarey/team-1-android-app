e24028741eda3bea8030e6579e765ffe87e60479
Jacob <jacobcarey95@googlemail.com>
2017-03-20 20:22:15 +0000

    Made it so the button stays turned on after leaving the settings activity, based on whether night mode is on. Also made it so the toggle button saves state across app.

--- app/src/main/java/com/project/one/team/musictheoryapp/SettingsActivity.java
+++ app/src/main/java/com/project/one/team/musictheoryapp/SettingsActivity.java
@@ -1,21 +1,46 @@
 package com.project.one.team.musictheoryapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.View;
+import android.widget.ToggleButton;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.getDefaultNightMode();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         getSupportActionBar().hide();
+
+        ToggleButton nmButton = (ToggleButton) findViewById(R.id.nightModeToggle);
+        if (((Theoryously) getApplication()).getNightMode()) {
+            nmButton.setChecked(true);
+        } else {
+            nmButton.setChecked(false);
+        }
+        nmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (((Theoryously) getApplication()).getNightMode()) {
+                    ((Theoryously) getApplication()).setNightMode(false);
+                } else {
+                    ((Theoryously) getApplication()).setNightMode(true);
+                }
+                recreate();
+            }
+        });
     }
+
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+        Intent i = new Intent(SettingsActivity.this, MainPageActivity.class);
+        startActivity(i);
         overridePendingTransition(R.anim.slide_right, R.anim.slide_left_out);
     }
 }
