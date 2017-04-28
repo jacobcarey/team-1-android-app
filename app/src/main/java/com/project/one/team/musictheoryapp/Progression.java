905e1bc71d6da970cfb638eb795e8572b531ad39
Oliver Hartley <o.hartley1@ncl.ac.uk>
2017-04-28 12:58:54 +0100

    send progress information to the firebase database (untested)

--- app/src/main/java/com/project/one/team/musictheoryapp/Progression.java
+++ app/src/main/java/com/project/one/team/musictheoryapp/Progression.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import static android.content.Context.MODE_PRIVATE;
 
 /**
@@ -28,26 +32,47 @@ public class Progression {
     public void increment(String difficulty) {
         SharedPreferences progression = context.getSharedPreferences("progression", MODE_PRIVATE);
         SharedPreferences.Editor editor = progression.edit();
-        editor.putInt(difficulty, progression.getInt(difficulty, 0)+1);
+
+        int newValue = progression.getInt(difficulty, 0)+1;
+        editor.putInt(difficulty, newValue);
         editor.commit();
+
+        final String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        final DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        database.child("users").child(userId).child("progression").child(difficulty).setValue(newValue);
+
         Toast.makeText(context.getApplicationContext(), "progression/"+difficulty+" is: "+progression.getInt(difficulty, 0), Toast.LENGTH_SHORT).show();
     }
 
     public void reset(String difficulty) {
         SharedPreferences progression = context.getSharedPreferences("progression", MODE_PRIVATE);
         SharedPreferences.Editor editor = progression.edit();
+
         editor.putInt(difficulty, 0);
         editor.commit();
+
+        final String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        final DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        database.child("users").child(userId).child("progression").child(difficulty).setValue(0);
+
         Toast.makeText(context.getApplicationContext(), "progression/"+difficulty+" is: "+progression.getInt(difficulty, 0), Toast.LENGTH_SHORT).show();
     }
 
     public void resetAll() {
         SharedPreferences prefs = context.getSharedPreferences("progression", MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
+
         editor.putInt("basic", 0);
         editor.putInt("intermediate", 0);
         editor.putInt("advanced", 0);
         editor.commit();
+
+        final String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        final DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        database.child("users").child(userId).child("progression").child("basic").setValue(0);
+        database.child("users").child(userId).child("progression").child("intermediate").setValue(0);
+        database.child("users").child(userId).child("progression").child("advanced").setValue(0);
+
         Toast.makeText(context.getApplicationContext(), "Reset all progression!", Toast.LENGTH_SHORT).show();
     }
 }
